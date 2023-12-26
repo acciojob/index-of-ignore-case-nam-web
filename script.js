1d3c029729ef3f3e5160386dfc61ac81d295c6f1
nam-web <65065658+nam-web@users.noreply.github.com>
2023-12-26 16:13:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	str1 = s1.toLowerCase();
+	str2 = s2.toLowerCase();
+	return str1.indexOf(str2);
+	// let m = str2.length;
+	// let n = str1.length;
+	// for(let i = 0; i < n-m; i++){
+	// 	if(str1.substring(i, i+n+1)===str2)
+	// 		return i;
+	// }
+	// return -1;
 }
 
 // Please do not change the code below
